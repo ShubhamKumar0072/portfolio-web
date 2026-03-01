6052e980fb211f084fb6b3852425f4ad4ed09d72
Shubham Kumar <shubhamkumar20040213@gmail.com>
2026-03-01 12:08:43 +0530

    make a collapsable navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,19 @@
 import "./Navbar.css";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Navbar() {
     const [theme, setTheme] = useState("light");
+    const [isOpen, setIsOpen] = useState(false);
+    const closeMenue = () => setIsOpen(false);
 
     const links = [
         { name: "Home", path: "/" },
         { name: "About", path: "/about" },
         { name: "Projects", path: "/projects" },
-        { name: "Connect", path: "/connect"}
+        { name: "Connect", path: "/connect" }
     ];
 
     useEffect(() => {
@@ -22,11 +26,20 @@ export default function Navbar() {
 
     return (
         <div className="Navbar">
-            <div className="nav-link">
+            <button
+                className="menue"
+                onClick={() => setIsOpen(!isOpen)}
+                aria-label="Toggle menu"
+            >
+                {isOpen ? <CloseIcon sx={{ color: "var(--accent)" }}/> : <MenuIcon sx={{ color: "var(--accent)" }}/>}
+            </button>
+
+            <div className={`nav-link ${isOpen? "active1" : ""}`}>
                 {links.map((item) => (
                     <NavLink
                         key={item.name}
                         to={item.path}
+                        onClick={closeMenue}
                         className={({ isActive }) =>
                             isActive ? "active" : ""
                         }
